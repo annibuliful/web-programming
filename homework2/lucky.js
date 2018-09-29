31e6a578338569e323167d8522d846bddc4358fb
lagmanzaza <lagmanmc@gmail.com>
2018-09-29 22:39:54 +0700

    homework 2

--- homework2/lucky.js
+++ homework2/lucky.js
@@ -1,7 +1,8 @@
+let listOfLuckNumbers = [45, 46, 78, 87, 39, 93, 56];
 /*
-* mock list of numbers that use to create lucky phone number
-*/
-function mockLuckyNumber(){
+ * mock list of numbers that use to create lucky phone number
+ */
+function mockLuckyNumber() {
   const work = [45, 46, 78, 87, 39, 93, 56];
   const love = [32, 24, 62, 36, 63, 69, 96];
   const wealth = [28, 82, 45, 54, 65, 56, 78];
@@ -12,50 +13,104 @@ function mockLuckyNumber(){
   return [work, love, wealth, merchandising, technology, educationAndWiseness];
 }
 
-/*
-* generate normal phone from list of lucky number
-*/
-function generateNormalPhoneNumber(){
+function unSelectNumber(value, element) {
+  if (element.checked === true) {
+    const index = listOfLuckNumbers.indexOf(value)
+    listOfLuckNumbers.splice(index, 1)
+  } else if (element.checked === false) {
+    listOfLuckNumbers.push(value)
+  }
 
-  // get all list of lucky numbers
-  const listOfLuckyNumber = mockLuckyNumber();
+}
+
+/*
+ * display data
+ * @param listIds {array} list of ids to render datas
+ * @param data {array} list
+ */
+function display(listIds, data) {
+  listIds.forEach((value, index) => {
+    document.getElementById(value).value = data[index]
+  });
+}
+
+/*
+ * generate normal phone from list of lucky number
+ */
+function generateNormalPhoneNumber() {
 
   //first 3 numbers
-  let result = '081';
+  let result = '081-';
 
   // contain numbers
   let countOfNumber = 7;
 
-  while(countOfNumber > 0){
-    if(countOfNumber === 1){
+  while (countOfNumber > 0) {
+    let random = Math.floor(Math.random() * 2)
+    if (random === 0) {
       const lastNumber = Math.floor(Math.random() * 10);
       result = result + lastNumber;
       countOfNumber -= 1;
-      break;
-    }else if(countOfNumber > 0){
-      const randomIndexOfCategory = Math.floor(Math.random() * 6);
-      const randomIndexOfNumber = Math.floor(Math.random() * 7);
-      result = result + listOfLuckyNumber[randomIndexOfCategory][randomIndexOfNumber];
+    } else if (random === 1) {
+      const randomIndex = Math.floor(Math.random() * listOfLuckNumbers.length)
+      result = result + listOfLuckNumbers[randomIndex];
       countOfNumber -= 2;
     }
+  }
+  if(result.length > 11){
+    return result.substr(0,11)
   }
   return result;
 }
 
-function onGeneratePhoneNumber(){
-  console.log('sss')
-  const number = generateNormalPhoneNumber();
-  display(['inputNumber'],[number])
+function generatePhoneNumberFromInput(number) {
+  const sumOfThreeNumbers = 9;
+  let containNumber = number - sumOfThreeNumbers;
+  let result = '081-';
+  for (let i = 1; i <= 7; i++) {
+    if (containNumber > 0) {
+      if(containNumber > 28){
+        const nextNumber = Math.floor((Math.random() * 9)+2)
+        console.log(nextNumber)
+        containNumber -= nextNumber;
+        result += nextNumber
+      }else if(containNumber <= 28){
+        const nextNumber = Math.floor((Math.random() * 9))
+        console.log(nextNumber)
+        containNumber -= nextNumber;
+        result += nextNumber
+      }
+    } else if (containNumber <= 0) {
+      result += '0';
+    }
+  }
+  return result
+}
+/*
+ * get value from user input
+ */
+function getInput() {
+  return document.getElementById('input').value;
 }
 
 /*
-* display to DOM
-* @param listIds {array} list of ids to render datas
-* @param data {array} list
-*/
-function display(listIds , data){
-  listIds.forEach((value, index)=>{
-    console.log(value)
-    document.getElementById(value).value = data[index]
-  })
+ * its sum equals to lucky number in the category you choose
+ */
+function generateFromNumber() {
+  const luckyNumber = getInput();
+  const listNumbers = [45, 46, 78, 87, 39, 93, 56];
+  const indexOfNumber = listNumbers.indexOf(Number(luckyNumber));
+  if (indexOfNumber !== -1) {
+    const result = generatePhoneNumberFromInput(luckyNumber)
+    console.log(result)
+  } else if (indexOfNumber !== -1) {
+
+  }
+}
+/*
+ * call this function when use click event to generate
+ */
+function onGeneratePhoneNumber() {
+  const number = generateNormalPhoneNumber();
+  display(['inputNumber'], [number])
 }
