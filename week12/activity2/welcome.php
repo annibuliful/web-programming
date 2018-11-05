736f74572017039d548b67a167518db1c78268c5
lagmanzaza <lagmanmc@gmail.com>
2018-11-05 13:50:52 +0700

    [Week12]

---
+++ week12/activity2/welcome.php
@@ -0,0 +1,20 @@
+<?php session_start();?>
+<!DOCTYPE html>
+ <html>
+    <head>
+        <title>Login and Registration Form</title>
+
+        <link rel="stylesheet" type="text/css" href="css/demo.css" />
+        <link rel="stylesheet" type="text/css" href="css/style.css" />
+		<link rel="stylesheet" type="text/css" href="css/animate-custom.css" />
+    </head>
+    <body>
+        <div class="container">
+
+        <?php
+            echo "<p style=\"font-weight: bold; font-size: 60px\">WELCOME TO OUR SYSTEM</p>"
+        ?>
+
+	</div>
+    </body>
+</html>
